eb73b72d12d867900f459da66aeff97d7733725c
lenagend <lenagend@naver.com>
2024-04-18 16:40:15 +0900

    feat(MessagingException): MessagingException 핸들러 추가
    message기능 추가에 따른 에러 핸들러 추가

--- src/main/java/com/everepl/evereplspringboot/controller/GlobalExceptionHandler.java
+++ src/main/java/com/everepl/evereplspringboot/controller/GlobalExceptionHandler.java
@@ -5,10 +5,10 @@ import com.everepl.evereplspringboot.exceptions.InvalidUrlException;
 import com.everepl.evereplspringboot.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.MessagingException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -47,6 +47,15 @@ public class GlobalExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("message", invalidUrlException.getMessage());
 
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<Object> handleMessagingException(MessagingException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.BAD_REQUEST);
+        body.put("error", "Messaging error");
+        body.put("message", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
